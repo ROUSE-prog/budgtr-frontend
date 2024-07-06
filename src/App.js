ef8846a36425c350c13299399e61d6513aa5366b
ROUSE-prog <stevenr2049@gmail.com>
2024-07-06 12:31:34 -0400

    Init Setup and App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import CreateTransaction from './pages/CreateTransaction';
+import EditTransaction from './pages/EditTransaction';
+import ShowTransaction from './pages/ShowTransaction';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/transactions/create" component={CreateTransaction} />
+          <Route path="/transactions/:id/edit" component={EditTransaction} />
+          <Route path="/transactions/:id" component={ShowTransaction} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
